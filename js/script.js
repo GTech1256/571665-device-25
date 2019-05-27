1043c914a7f1bc165eeb90a8f86a994e36bc3191
Roman <pomah156@gmail.com>
2019-05-28 00:23:52 +0300

    Added styles for header

--- js/script.js
+++ js/script.js
@@ -1,12 +1,16 @@
+/*
+const moreLinksBtn = document.getElementsByClassName('page-header__links-more')[0]
 const subNav = document.getElementsByClassName('page-header__sub-nav')[0]
 
 const ACTIVE_SUB_NAV = 'page-header__sub-nav_active'
+const ACTIVE_MORE_LINKS_BTN = 'page-header__links-more_active'
 
 function revertSubNavigation() {
-  console.log(subNav);
   if (subNav.classList.contains(ACTIVE_SUB_NAV)) {
+    moreLinksBtn.classList.remove(ACTIVE_MORE_LINKS_BTN);
     subNav.classList.remove(ACTIVE_SUB_NAV);
   } else {
+    moreLinksBtn.classList.add(ACTIVE_MORE_LINKS_BTN);
     subNav.classList.add(ACTIVE_SUB_NAV);
   };
 }
@@ -26,3 +30,5 @@ if (document.readyState !== 'loading') {
 } else {
   document.addEventListener('DOMContentLoaded', init);
 }
+
+*/
