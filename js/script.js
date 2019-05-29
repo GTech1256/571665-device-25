00bbefb138118d4fc6fb6b979e4229d649581413
Roman <you@example.com>
2019-05-29 17:56:22 +0300

    added styles of positions for index.html

--- js/script.js
+++ js/script.js
@@ -18,12 +18,16 @@ function revertSubNavigation() {
 
 
 
+
+
+
  function init() {
   console.log('js inited');
   document
     .getElementsByClassName('page-header__links-more')[0]
     .addEventListener('click', revertSubNavigation);
 }
+
 
 if (document.readyState !== 'loading') {
   init();
@@ -32,3 +36,39 @@ if (document.readyState !== 'loading') {
 }
 
 */
+
+
+
+
+
+
+
+
+
+/* SLIDER CONTROLLER  */
+function setSliderFrame({ target }) {
+
+  const firstNodeOfSlider = document.getElementsByClassName('slider__item')[0];
+
+  if (!target.classList.contains('slider__nav-item') && !target.dataset.sliderNumber) {
+    return;
+  }
+
+
+  firstNodeOfSlider.style.marginLeft = `-${(target.dataset.sliderNumber -1) * 1160}px`;
+}
+
+
+
+ function init() {
+  console.log('js inited');
+  document
+    .getElementsByClassName('slider__nav')[0]
+    .addEventListener('click', setSliderFrame);
+}
+
+if (document.readyState !== 'loading') {
+  init();
+} else {
+  document.addEventListener('DOMContentLoaded', init);
+}
