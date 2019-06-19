3bc05471d6a7f73b283b44003b37869e54c9bb70
Roman <you@example.com>
2019-06-19 20:23:59 +0300

    Added basic logic of opening form & map

--- js/script.js
+++ js/script.js
@@ -1,46 +1,5 @@
-/*
-const moreLinksBtn = document.getElementsByClassName('page-header__links-more')[0]
-const subNav = document.getElementsByClassName('page-header__sub-nav')[0]
-
-const ACTIVE_SUB_NAV = 'page-header__sub-nav_active'
-const ACTIVE_MORE_LINKS_BTN = 'page-header__links-more_active'
-
-function revertSubNavigation() {
-  if (subNav.classList.contains(ACTIVE_SUB_NAV)) {
-    moreLinksBtn.classList.remove(ACTIVE_MORE_LINKS_BTN);
-    subNav.classList.remove(ACTIVE_SUB_NAV);
-  } else {
-    moreLinksBtn.classList.add(ACTIVE_MORE_LINKS_BTN);
-    subNav.classList.add(ACTIVE_SUB_NAV);
-  };
-}
-
-
-
-
-
-
-
- function init() {
-  console.log('js inited');
-  document
-    .getElementsByClassName('page-header__links-more')[0]
-    .addEventListener('click', revertSubNavigation);
-}
-
-
-if (document.readyState !== 'loading') {
-  init();
-} else {
-  document.addEventListener('DOMContentLoaded', init);
-}
-
-*/
-
-
-
-
-
+const firstServiceDescription = document.getElementsByClassName('services__desc-item')[0]
+const servicesStepPixels = 1010; // width of element
 
 
 
@@ -50,6 +9,7 @@ function setSliderFrame({ target }) {
 
   const firstNodeOfSlider = document.getElementsByClassName('slider__item')[0];
 
+  // if target !== input[type=radio,data-slider-number="1"]
   if (!target.classList.contains('slider__nav-item') && !target.dataset.sliderNumber) {
     return;
   }
@@ -58,9 +18,6 @@ function setSliderFrame({ target }) {
   firstNodeOfSlider.style.marginLeft = `-${(target.dataset.sliderNumber -1) * 1160}px`;
 }
 
-
-const firstServiceDescription = document.getElementsByClassName('services__desc-item')[0]
-const servicesStepPixels = 1010; // width of element
 /* SERVICES CONTROLLER */
 function setServiceSlider({ target }, activeClassName) {
   target.classList.add(activeClassName);
@@ -73,7 +30,8 @@ function clearActiveClass(items, activeClassName) {
   }
 }
 
- function init() {
+// MAIN
+function init() {
   console.log('js inited');
   document
     .getElementsByClassName('slider__nav')[0]
@@ -81,12 +39,60 @@ function clearActiveClass(items, activeClassName) {
 
   const activeClassName = 'services__item_active';
   const serviceItems = document.getElementsByClassName('services__item');
+
+  /* MODAL Variables */
+  const modalActiveClass = 'modal_display_block';
+
+  /* MODAL MAP NODES */
+  const mapPopup =      document.querySelector('.modal__map-popup');
+  const mapButtonOpen = document.querySelector('.information__map-link');
+  const mapButtonClose = mapPopup.querySelector('.modal__close');
+  
+  /* MODAL FORM NODES */
+  const formPopup =       document.querySelector('.modal__wrtite-us');
+  const formButtonOpen =  document.querySelector('.information__write-us-button');
+  const formButtonClose = formPopup.querySelector('.modal__close');
+
+
+
+  /* EVENTS */
+
+  /* services events */
   for (const item of serviceItems) {
-    item.addEventListener('click', (event) => {
+    item.addEventListener('click', (evt) => {
       clearActiveClass(serviceItems, activeClassName)
-      setServiceSlider(event, activeClassName)
+      setServiceSlider(evt, activeClassName)
     })
   }
+
+  /* modal-map events */
+  //      OPEN
+  mapButtonOpen.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    
+    mapPopup.classList.add(modalActiveClass);
+  })
+  //     CLOSE
+  mapButtonClose.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    
+    mapPopup.classList.remove(modalActiveClass);
+  })
+
+  /* modal-form events */
+  //      OPEN
+  formButtonOpen.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    
+    formPopup.classList.add(modalActiveClass);
+  })
+  //     CLOSE
+  formButtonClose.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    
+    formPopup.classList.remove(modalActiveClass);
+  })
+
 }
 
 if (document.readyState !== 'loading') {
